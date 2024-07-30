d7d17657951325a4c71be7c964ddd38e0baca088
Manoj-B-S <bsmanoj125@gmail.com>
2024-07-30 23:02:01 +0530

    added the login auth and other css stylings

---
+++ js/script.js
@@ -0,0 +1,58 @@
+let nav = document.querySelector('.nav');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+   loginForm.classList.add('active');
+}
+
+document.querySelector('#close-login-form').onclick = () =>{
+   loginForm.classList.remove('active');
+}
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .nav');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   loginForm.classList.remove('active');
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
+
+   if(window.scrollY > 0){
+      document.querySelector('.header').classList.add('active');
+   }else{
+      document.querySelector('.header').classList.remove('active');
+   }
+}
